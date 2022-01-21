4feb66d829f585eb1066f0b89afbe5c374114e24
Jesse Ferreira <j.fnascimento@vermon.com>
2022-01-21 15:30:15 +0100

    first commit

---
+++ Block.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 21 14:26:39 2022
+
+@author: J.FNASCIMENTO
+"""
+
+
+from hashlib import sha256
+import json
+
+class Block:
+    def __init__(self, index, transactions, timestamp, previous_hash,nonce=0):
+        self.index = index
+        self.transactions = transactions
+        self.timestamp = timestamp
+        self.previous_hash = previous_hash
+        self.nonce = nonce
+    
+    def compute_hash(self):
+        block_string = json.dumps(self.__dict__,sort_keys=True)
+        return sha256(block_string.encode()).hexdigest()
+    
+    
---
+++ Blockchain.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 21 15:22:11 2022
+
+@author: J.FNASCIMENTO
+"""
+
+import time
+import Block
+
+class Blockchain:
+    def __init__(self):
+        self.unconfirmed_transctions = []
+        self.chain = []
+        self.create_genesis_block()
+        
+    def create_genesis_block(self):
+        genesis_block = Block(0,[],time.time(), "0")
+        genesis_block.hash = genesis_block.compute_hash()
+        self.chain.append(genesis_block)
+        
+    @property
+    def last_block(self):
+        return self.chain[-1]
